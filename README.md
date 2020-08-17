9d8aab2dc42034a10a0967fbaac615e51e568cff
Anon <N/A>
2020-08-17 13:58:49 -0400

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# AlertUser
+Alert the user when a new tab/window is created or removed.
+
+## Usage
+1. Open the Chrome extension management page -> `chrome://extensions`
+2. Enable developer mode on the top right of the page.
+3. Click the `load unpacked` button on the top left of the page.
+4. Select the root folder of this project.
---
+++ background.js
@@ -0,0 +1,52 @@
+let loadedSounds = new Map()
+let enabled = false
+
+function playAudio(soundName) {
+  let sound = loadedSounds.get(soundName)
+  if (!sound) {
+    try {
+      sound = new Audio(chrome.runtime.getURL(`./sounds/${soundName}`))
+      loadedSounds.set(soundName, sound)
+    } catch (err) {
+      speechSynthesis.speak(new SpeechSynthesisUtterance(`failed to load ${soundName}`))
+      return 
+    }
+  } 
+
+  sound.currentTime = 0
+  sound.play()
+}
+
+
+
+chrome.tabs.onCreated.addListener(() => {
+  if (!enabled) return
+  playAudio("good.wav")
+  // speechSynthesis.speak(new SpeechSynthesisUtterance("created"))
+})
+
+chrome.tabs.onRemoved.addListener(() => {
+  if (!enabled) return
+  playAudio("bad.wav")
+  // speechSynthesis.speak(new SpeechSynthesisUtterance("removed"))
+})
+
+chrome.windows.onCreated.addListener(() => {
+  if (!enabled) return
+  playAudio("good.wav")
+  // speechSynthesis.speak(new SpeechSynthesisUtterance("new window"))
+})
+
+chrome.windows.onRemoved.addListener(() => {
+  if (!enabled) return
+  playAudio("bad.wav")
+  // speechSynthesis.speak(new SpeechSynthesisUtterance("removed window"))
+})
+
+
+chrome.browserAction.onClicked.addListener(() => {
+  enabled = !enabled
+  chrome.browserAction.setIcon({path: enabled ? {"128": `icons/128.png`} : {"128": `icons/128g.png`}})
+})
+
+
